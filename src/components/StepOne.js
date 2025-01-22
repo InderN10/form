d9c517c41bc417d5a89d59372853de9b8ba2ea7e
InderN10 <midtulah2000@gmail.com>
2025-01-22 09:56:25 +0800

    style-done

--- src/components/StepOne.js
+++ src/components/StepOne.js
@@ -1,17 +1,45 @@
 import React from 'react'
 import PineLogo from '@/icons/PineconeLogo';
+
 function StepOne(props) {
     const { handleNextStep, handleBackStep } = props;
     return (
-        <div>
-            <div className=''>
-                <p className='text-red-600'>first step</p>
-                <div>
-                    <PineLogo/>
+        <div className='w-screen h-screen flex flex-col justify-center items-center bg-slate-100'>
+            <div className='w-[480px] h-[655px] bg-white p-8 rounded-[8px]'>
+                <div className='flex flex-col gap-[8px] mb-[28px]'>
+                    <div>
+                        <PineLogo />
+                    </div>
+                    <p className='text-[26px] font-semibold leading-[31,5px]'>Join Us!</p>
+                    <p className='text-gray-400 text-[18px] font-normal'>Please provide all current information accurately.</p>
                 </div>
-                <p>Join us!</p>
-                <p>Please provide all current information accurately.</p>
-                <button onClick={handleNextStep}>next</button>
+
+                <div className='flex flex-col gap-[8px] mb-[12px]'>
+                    <div className='flex'>
+                        <p>First name</p>
+                        <p className='text-red-600'>*</p>
+                    </div>
+                    <input className='w-[416px] h-[44px] border rounded-[10px] p-[5px]' placeholder='Your first name'></input>
+                </div>
+                <div className='flex flex-col gap-[8px] mb-[12px]'>
+                    <div className='flex'>
+                        <p>Last name</p>
+                        <p className='text-red-600'>*</p>
+                    </div>
+                    <input className='w-[416px] h-[44px] border rounded-[10px] p-[5px] ' placeholder='Your last name'></input>
+                </div>
+                <div className='flex flex-col gap-[8px]'>
+                    <div className='flex'>
+                        <p>Username</p>
+                        <p className='text-red-600'>*</p>
+                    </div>
+                    <input className='w-[416px] h-[44px] border rounded-[10px] p-[5px]' placeholder='Your usename'></input>
+                </div>
+                <div >
+                    <button 
+                    className='h-[44px] w-[416px] rounded-[6px] text-white bg-black mt-[135px] flex justify-center items-center' 
+                    onClick={handleNextStep}>Continue</button>
+                </div>
             </div>
         </div>
     )
--- src/components/StepTwo.js
+++ src/components/StepTwo.js
@@ -1,12 +1,55 @@
 import React from 'react'
-
+import PineLogo from '@/icons/PineconeLogo';
 function StepTwo(props) {
     const { handleNextStep, handleBackStep } = props;
     return (
-        <div>
-            <p>steptwo</p>
-            <button onClick={handleBackStep}>back</button>
-            <button onClick={handleNextStep}>next</button>
+
+        <div className='w-screen h-screen flex flex-col justify-center items-center bg-slate-100'>
+            <div className='w-[480px] h-[655px] bg-white p-8 rounded-[8px]'>
+                <div className='flex flex-col gap-[8px] mb-[28px]'>
+                    <div>
+                        <PineLogo />
+                    </div>
+                    <p className='text-[26px] font-semibold leading-[31,5px]'>Join Us!</p>
+                    <p className='text-gray-400 text-[18px] font-normal'>Please provide all current information accurately.</p>
+                </div>
+                <div className='flex flex-col gap-[8px] mb-[12px]'>
+                    <div className='flex'>
+                        <p>Email</p>
+                        <p className='text-red-600'>*</p>
+                    </div>
+                    <input className='w-[416px] h-[44px] border rounded-[10px] p-[5px]' placeholder='Your email'></input>
+                </div>
+                <div className='flex flex-col gap-[8px] mb-[12px]'>
+                    <div className='flex'>
+                        <p>Phone number</p>
+                        <p className='text-red-600'>*</p>
+                    </div>
+                    <input className='w-[416px] h-[44px] border rounded-[10px] p-[5px] ' placeholder='Your phone number'></input>
+                </div>
+                <div className='flex flex-col gap-[8px] mb-[12px]'>
+                    <div className='flex'>
+                        <p>Password</p>
+                        <p className='text-red-600'>*</p>
+                    </div>
+                    <input className='w-[416px] h-[44px] border rounded-[10px] p-[5px]' placeholder='Your password'></input>
+                </div>
+                <div className='flex flex-col gap-[8px]'>
+                    <div className='flex'>
+                        <p>Comfirm password</p>
+                        <p className='text-red-600'>*</p>
+                    </div>
+                    <input className='w-[416px] h-[44px] border rounded-[10px] p-[5px]' placeholder='Comfirm password'></input>
+                </div>
+                <div className='flex mt-[50px] g-[8px]'>
+                    <button
+                        className='h-[44px] w-[128px] rounded-[6px] border text-black flex justify-center items-center'
+                        onClick={handleBackStep}>Back</button>
+                    <button
+                        className='h-[44px] w-[280px] rounded-[6px] border text-white bg-black flex justify-center items-center'
+                        onClick={handleNextStep}>Continue</button>
+                </div>
+            </div>
         </div>
     )
 }
